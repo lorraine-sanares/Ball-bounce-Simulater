f819ff4c4efd8691855e0485384c7dbd3eb2dd91
Lorraine Sanares <96808637+lorraine-sanares@users.noreply.github.com>
2025-03-18 16:38:12 +1100

    screen

--- ball_bounce.py
+++ ball_bounce.py
@@ -5,7 +5,7 @@ import pygame,time,random
 pygame.init()
 
 #setting screen size of pygame window to 800 by 600 pixels
-screen=pygame.display.set_mode((800,600))
+screen=pygame.display.set_mode((800,500))
 background=pygame.image.load('background-img.jpg')
 #Adding title
 pygame.display.set_caption('Ball Bounce Simulation')
